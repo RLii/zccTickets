40304721df949cae217097e2ad66b0433587b0d8
Richard <rlii@uwo.ca>
2021-11-29 00:21:31 -0500

    resolved cors errers

--- client/src/Components/LandingPage.js
+++ client/src/Components/LandingPage.js
@@ -2,6 +2,9 @@ import { useEffect } from "react"
 import axios from "axios"
 function LandingPage() {
     useEffect(()=>{
+        axios.get("http://localhost:3001/getTickets")
+        .then(res =>{ console.log(res)})
+        .catch(err=>{console.log(err)})
         console.log("landingpageuseeffect");
     }, [])
     return(<p>LandingPage</p>)
--- server/index.js
+++ server/index.js
@@ -1,27 +1,32 @@
 const express = require("express");
 const axios = require("axios")
+const cors = require("cors")
 
 const PORT = process.env.PORT || 3001
 
 const app = express();
+app.use(cors())
+
+app.use(express.static("../client/src"))
 
 const token = Buffer.from("richlii@outlook.com/token:6lZGByHibQEd2eJUWscFFTPcrKINrPxvHHcJB4Xk").toString("base64")
 
 app.get("/getTickets", async (req, res) => {
     const url = "https://zccliitickets.zendesk.com/api/v2/tickets";
     try{
-        const res = await axios({
+        const result = await axios({
             method: 'get',
             url: url,
             headers:{"Authorization" : "Basic " + token}
         })
-        console.log("toot",res)
+        console.log("toot",result.data.tickets)
+        res.json({tickets:result.data.tickets})
     }
     catch(err){
         console.log("monkah")
         console.log(err, "tee")
+        res.json({error: err.data})
     }
-    res.json({message: "from server"})
 })
 
 app.listen(PORT, ()=>{
